a8ee041098ceac581949cd730d0a9b75deff8f42
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-07-15 08:39:24 +0200

    feat: move page generation to JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import { pageload } from "./page.js";
+import img from "./cafe.png";
+
+pageload(img);
 console.log("Hello hello restaurant page")
---
+++ src/page.js
@@ -0,0 +1,16 @@
+export function pageload(img) {
+  const contentDiv = document.querySelector("#content");
+
+  const title = document.createElement("h1");
+  title.innerText = "Iban's Hut";
+  contentDiv.appendChild(title);
+
+  const description = document.createElement("p");
+  description.innerText = `Fresh, healthy food served steps from the sea. At our cozy beach café, we mix rustic vibes with local ingredients for simple, feel-good meals. Come relax, eat well, and enjoy the ocean breeze.`
+  contentDiv.appendChild(description);
+
+  const homeImg = document.createElement("img");
+  homeImg.src = img;
+  homeImg.alt = "Picture of Iban's hut";
+  contentDiv.appendChild(homeImg);
+}
