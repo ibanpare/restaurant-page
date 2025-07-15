5a736d6abb0acde9bd77a136ba563dae6eab1f62
ibanpare <13278805+ibanpare@users.noreply.github.com>
2025-07-15 21:03:24 +0200

    feat: setup basic navigation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-import home from "./home.js";
+import loadHome from "./home.js";
 import loadMenu from "./menu.js";
 import loadAbout from "./about.js";
 
-home();
-loadMenu();
-loadAbout();
+loadHome();
+
+const contentDiv = document.querySelector("#content");
+
+const homeButton = document.querySelector("#home");
+homeButton.addEventListener("click", () => {
+    cleanPage();
+    loadHome();
+})
+
+const aboutButton = document.querySelector("#about");
+aboutButton.addEventListener("click", () => {
+    cleanPage();
+    loadAbout();
+})
+
+const menuButton = document.querySelector("#menu");
+menuButton.addEventListener("click", () => {
+    cleanPage();
+    loadMenu();
+})
+
+function cleanPage(){
+    let keepGoing = true;
+    while(keepGoing) {
+        if(contentDiv.firstChild) {
+            contentDiv.removeChild(contentDiv.firstChild);
+        }
+        else keepGoing = false;
+    }
+}
